7845063a8044f241e9110b7b570a82864e114633
Jacky <65343734+jakitun@users.noreply.github.com>
2024-01-02 08:08:55 +0700

    dynamic time

---
+++ js/preloader.js
@@ -0,0 +1,11 @@
+// script.js
+window.addEventListener('load', () => {
+    const preloader = document.querySelector('.preloader');
+    const content = document.querySelector('.content');
+
+    // Hide the preloader
+    preloader.style.display = 'none';
+
+    // Show the content
+    content.style.display = 'block';
+});
